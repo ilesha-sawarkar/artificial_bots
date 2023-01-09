1c4dd11aa971139bb6e8d0820ec75185baa4f9d7
ilesha-sawarkar <ileshasawarkar2024@u.northwestern.edu>
2023-01-09 15:05:40 -0600

    many links

--- generate.py
+++ generate.py
@@ -1,5 +1,37 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+
+
+pyrosim.Start_SDF("boxes.sdf")
+x=0
+y=0
+z=1
+length =1 
+width =1
+height=1
+
+for i in range(0,5):
+	
+	for j in range(0,5):
+		for k in range(0,10):
+			pyrosim.Send_Cube(name="Box", pos=[x,y, z] , size=[length, width, height])
+			z=z+1
+			length= length*90/100
+			width= width*90/100
+			height= height*90/100
+		y=y+1
+		z=1
+		length = 1
+		width= 1
+		height=1
+		
+	y=0
+	x=x+1
+			
+
+	#x=2
+	#y=2
+	#z=2
+#pyrosim.Send_Cube(name="Box2", pos=[x,y, z] , size=[length, width, height])
+
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -2,10 +2,15 @@
 
 import pybullet as p
 import time
+import pybullet_data
+
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-p.loadSDF("box.sdf")
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
 for i in range (0,1000):
 	p.stepSimulation()
 	time.sleep(1/60)
