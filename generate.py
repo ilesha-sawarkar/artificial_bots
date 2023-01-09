5beb7b0a28ce220f643662f751f74710311dd4a5
ilesha-sawarkar <ileshasawarkar2024@u.northwestern.edu>
2023-01-09 15:12:44 -0600

    commit world

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@ import pyrosim.pyrosim as pyrosim
 
 
 
-pyrosim.Start_SDF("boxes.sdf")
+pyrosim.Start_SDF("world.sdf")
 x=0
 y=0
 z=1
@@ -10,24 +10,7 @@ length =1
 width =1
 height=1
 
-for i in range(0,5):
-	
-	for j in range(0,5):
-		for k in range(0,10):
-			pyrosim.Send_Cube(name="Box", pos=[x,y, z] , size=[length, width, height])
-			z=z+1
-			length= length*90/100
-			width= width*90/100
-			height= height*90/100
-		y=y+1
-		z=1
-		length = 1
-		width= 1
-		height=1
-		
-	y=0
-	x=x+1
-			
+pyrosim.Send_Cube(name="World", pos=[x,y, z] , size=[length, width, height])
 
 	#x=2
 	#y=2
--- simulate.py
+++ simulate.py
@@ -10,7 +10,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("boxes.sdf")
+p.loadSDF("world.sdf")
 for i in range (0,1000):
 	p.stepSimulation()
 	time.sleep(1/60)
