1ff92656a9887dcfc844408eef942a1af1082d2f
ilesha-sawarkar <ileshasawarkar2024@u.northwestern.edu>
2023-01-09 15:29:29 -0600

    commit

--- generate.py
+++ generate.py
@@ -1,20 +1,35 @@
 import pyrosim.pyrosim as pyrosim
 
-
-
-pyrosim.Start_SDF("world.sdf")
-x=0
-y=0
-z=1
 length =1 
 width =1
 height=1
 
-pyrosim.Send_Cube(name="Box", pos=[x,y, z] , size=[length, width, height])
+def Create_World():
+	x=0
+	y=0
+	z=0.5
+	pyrosim.Start_SDF("world.sdf")
+	
+	
+	pyrosim.Send_Cube(name="Box", pos=[x,y, z] , size=[length, width, height])
 
 	#x=2
 	#y=2
 	#z=2
-#pyrosim.Send_Cube(name="Box2", pos=[x,y, z] , size=[length, width, height])
+	#pyrosim.Send_Cube(name="Box2", pos=[x,y, z] , size=[length, width, height])
+	
+	pyrosim.End()
+	
+def Create_Robot():
+	x=2
+	y=2
+	z=0.5
+	pyrosim.Start_URDF("body.urdf")
+	pyrosim.Send_Cube(name="Torso", pos=[x,y, z] , size=[length, width, height])
+	pyrosim.Send_Cube(name="Leg", pos=[1,0,1.5] , size=[length, width, height])
+	pyrosim.End()
+	
 
-pyrosim.End()
+
+Create_World()
+Create_Robot()
--- simulate.py
+++ simulate.py
@@ -10,6 +10,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 for i in range (0,1000):
 	p.stepSimulation()
