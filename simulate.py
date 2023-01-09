7f72cc97d3ddc0d6479fe4f0d8944beac1540cbc
ilesha-sawarkar <ileshasawarkar2024@u.northwestern.edu>
2023-01-09 11:11:28 -0600

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
 import pybullet as p
+import time
 
-pass
+pLuhysicsClient = p.connect(p.GUI)
+
+for i in range (0,1000):
+	p.stepSimulation()
+	time.sleep(1/60)
+p.disconnect()
