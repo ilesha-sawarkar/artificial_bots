c3270bcfcc42a065d8e455f7f53ef8261646c93f
ilesha-sawarkar <ileshasawarkar2024@u.northwestern.edu>
2023-01-22 14:28:12 -0600

    commit

--- robot.py
+++ robot.py
@@ -49,7 +49,7 @@ class ROBOT :
 			if self.nn.Is_Motor_Neuron(neuronName):
 				jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)    
 				desiredAngle = self.nn.Get_Value_Of(neuronName)   
-				print(jointName, desiredAngle)
+				#print(jointName, desiredAngle)
 		
 				self.motors[jointName].Set_Value(self.robotId, desiredAngle)
 				#print(neuronName)
