bb46db4ccabb8e6c2948333f7146a25b8fc16bb8
ilesha-sawarkar <ileshasawarkar2024@u.northwestern.edu>
2023-01-23 00:02:16 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -41,7 +41,9 @@ def Generate_Brain():
 	pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = 1.0 )
 	pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1.0 )
 	# add more synapses and weights
-	
+	pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4, weight = 1.0 )
+	pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = 1.0 )
+	pyrosim.Send_Synapse( sourceNeuronName = 0, targetNeuronName = 4 , weight = 1.0 )
 	
 	pyrosim.End()
 	
